0d6b517a7f7a72ddab09784180fafc8a150e8736
sebug <sebastian.gfeller@gmail.com>
2017-11-27 22:10:29 +0100

    Calculating text to hash

--- ConvertToEPTrigger/index.js
+++ ConvertToEPTrigger/index.js
@@ -5,6 +5,33 @@ module.exports = function (context, req) {
     context.log('The external application code is ' + process.env.ExternalApplicationCode);
     context.log('The POST URL is ' + process.env.PostURL);
 
+
+    const identifier = '';
+    const preSharedKey = process.env.PreSharedKey;
+    const firstName = req.body.firstname;
+    const lastName = req.body.surname;
+    const curdate = new Date();
+    const utcYear = curdate.getUTCFullYear();
+    let utcMonth = curdate.getUTCMonth() + 1;
+    if (utcMonth < 10) {
+	utcMonth = "0" + utcMonth;
+    }
+    let utcDay = curdate.getUTCDate();
+    if (utcDay < 10) {
+	utcDay = "0" + utcDay;
+    }
+    let utcHour = curdate.getUTCHours();
+    if (utcHour < 10) {
+	utcHour = "0" + utcHour;
+    }
+    const dateTimeShort = "" + utcYear + utcMonth + utcDay + utcHour;
+    
+    const textToHash = '' + preSharedKey + firstName + lastName + '' + identifier + '' + dateTimeShort;
+
+    context.log('Text to hash is ' + textToHash);
+
+
+
     context.res = {
 	message: 'Converted'
     };
