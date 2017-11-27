41ecb82db0b3fe8032afacd13ebcbd221a48a51b
sebug <sebastian.gfeller@gmail.com>
2017-11-27 21:53:15 +0100

    All secret parameters

--- ConvertToEPTrigger/index.js
+++ ConvertToEPTrigger/index.js
@@ -2,6 +2,8 @@ module.exports = function (context, req) {
     context.log('Converting to External POST ' + JSON.stringify(req.body));
 
     context.log('The pre-shared key is ' + process.env.PreSharedKey);
+    context.log('The external application code is ' + process.env.ExternalApplicationCode);
+    context.log('The POST URL is ' + process.env.PostURL);
 
     context.res = {
 	message: 'Converted'
