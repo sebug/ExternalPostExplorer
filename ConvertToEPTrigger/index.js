2e375e763584d5e592828f2cdd83759d07b2050b
sebug <sebastian.gfeller@gmail.com>
2017-11-27 21:48:29 +0100

    Pre-shared key is a setting

--- ConvertToEPTrigger/index.js
+++ ConvertToEPTrigger/index.js
@@ -1,6 +1,8 @@
 module.exports = function (context, req) {
     context.log('Converting to External POST ' + JSON.stringify(req.body));
 
+    context.log('The pre-shared key is ' + process.env.PreSharedKey);
+
     context.res = {
 	message: 'Converted'
     };
